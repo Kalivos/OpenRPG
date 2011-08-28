4d68e5765acfdaaf657a4ae4d8d317927b977533
Tim Withers <timwithers@gmail.com>
2011-08-28 10:34:53 -0700

    Added placeholder config options to config.php Modified dbSetup() to perform updates based on file existance. writeTest() modified to also check if file is missing. writeDBConfig() modified to use "$this->configFile" rather than a hardcoded config filename. Added function writeGoogleAPI($key) that will save a key to the config file.

--- setup/config.php
+++ setup/config.php
@@ -7,4 +7,17 @@ define("DATABASE_NAME", "");
 define("DATABASE_USER", "");
 define("DATABASE_PW", "");
 
+//force ssh?
+define("SSH", "0");
+
+//users will still be able to access the outermost website when set to false
+//admins bypass this restriction
+define("ALLOW_LOGIN", "0");
+
+//default CSS theme
+define("THEME", "");
+
+//google api key
+define("GOOGLE_API", "");
+
 ?>
--- setup/setup.class.php
+++ setup/setup.class.php
@@ -2,6 +2,21 @@
 
 class Setup
 {
+	private $configFile = "config.php";
+	
+	//Returns an array of directories in ascending order
+	private function dir_list($dir)
+	{
+		$list = new array();
+		foreach(array_diff(scandir($dir),array('.','..')) as $file)
+		{
+			if(is_dir($dir.'/'.$file))
+			{
+				$list[]=$file;
+			}
+		}
+		return $list;
+	}
 
 	//test if we can connect to the database
 	function dbTest($dbhost, $dbuser, $dbpass, $dbname, $dbport="3306")
@@ -16,36 +31,87 @@ class Setup
 			{
 				$status = true;
 			}
+			
+			mysql_close($conn);
 		}
 		
 		return $status;
 	}
 	
-	function promptDBinfo()
-	{
-		
-	}
-	
 	//setup the database
 	function dbSetup($dbhost, $dbuser, $dbpass, $dbname, $dbport="3306")
 	{
-		$conn = mysql_connect($dbhost.":".$dbport, $dbuser, $dbpass) or die("Error connecting to database. ".mysql_error());
-		mysql_select_db($dbname, $conn) or die("Error selecting database. ".mysql_error());
+		$db_major = 00;
+		$db_minor = 00;
+		$db_build = 00;
+		$installed = "01/01/2010";
 		
-		//TODO:
-		//This needs to be rewritten to SOURCE update files.
+		require_once($this->configFile);
+
+		$conn = mysql_connect($dbhost.":".$dbport, $dbuser, $dbpass);
+		
+		if($conn)
+		{
+			mysql_select_db($dbname, $conn);
+			
+			$result = mysql_query("SELECT major, minor, build, installed FROM version ORDER BY installed DESC, LIMIT 1");
+			
+			while ($row = mysql_fetch_array($result, MYSQL_ASSOC))
+			{
+				$db_major = $row['major'];
+				$db_minor = $row['minor'];
+				$db_build = $row['build'];
+				$installed = $row['installed'];
+			}
+			
+			mysql_free_result($result);	
+		}
+		
 		//Files will be located under SQL directory followed by version number and finally, patch date.
 		//Example: SQL/01.00.00/2011.08.03
 		//Files will be sorted ascending order and will be processed in that fashion
 		//Latest version number and date of last update will be stored in database
-		mysql_query("SOURCE setup.sql") or die("Error setting up database. ".mysql_error());
+		$db_version = $db_major . $db_minor . $db_build;
+		foreach(dir_list("SQL") as $version)
+		{				
+			if($db_version <= $version)
+			{ 
+				//get all SQL files with a .sql extension.
+				$updates = glob("SQL/".$version. "/*.sql");
+				
+				foreach($updates as $updateScript)
+				{
+					if($updateScript > $installed )
+					{
+						mysql_query("SOURCE SQL/".$version."/".$updateScript) or die("Error setting up database.<br />\nFile: SQL/".$version."/".$updateScript."<br />\n".mysql_error());
+					}
+				}
+			}
+			
+			//now that the sql file has been sourced, it is the latest version
+			$db_version = $version;
+		}
+		
+		list($major, $minor, $build) = explode($db_version);
+		mysql_query("INSERT INTO version (installed, major, minor, build) VALUES (CURRENT_DATE(), $major, $minor, $build)");
+		
+		mysql_close($conn);	
 	}
 	
 	//test to check if config is writable
 	function writeTest()
 	{
 		$status = false;
-		$fh = fopen("config.php", 'a+');
+		$fh = false;
+		
+		if(file_exists($this->configFile))
+		{
+			$fh = fopen($this->configFile, 'a');
+		}
+		else
+		{
+			die($this->configFile." not found. Try placing ".$this->configFile." into the setup directory.");
+		}
 		
 		if($fh)
 		{
@@ -55,10 +121,10 @@ class Setup
 		return $status;
 	}
 	
-	//save config file
-	function writeConfig($dbhost, $dbport, $dbuser, $dbpass, $dbname)
+	//save db config
+	function writeDBConfig($dbhost, $dbport, $dbuser, $dbpass, $dbname)
 	{
-		$config = file_get_contents("config.php");
+		$config = file_get_contents($this->configFile);
 		
 		//write dbName
 		$config = preg_replace('/DATABASE_ADDRESS",\s*"[^"]*/', 'DATABASE_ADDRESS", "'.$dbhost, $config);
@@ -76,7 +142,21 @@ class Setup
 		$config = preg_replace('/DATABASE_NAME",\s*"[^"]*/', 'DATABASE_NAME", "'.$dbname, $config);
 		
 		//save
-		file_put_contents("config.php", $config);
+		file_put_contents($this->configFile, $config);	
+		
+	}
+	
+	
+	//saves google api key
+	function writeGoogleAPI($key)
+	{
+		$config = file_get_contents($this->configFile);
+		
+		//write API key
+		$config = preg_replace('/GOOGLE_API",\s*"[^"]*/', 'GOOGLE_API", "'.$key, $config);
+		
+		//save
+		file_put_contents($this->configFile, $config);	
 	}
 	
 	
