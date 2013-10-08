2028db8711f8cb0f89aaae2759559725e0e562e3
Tim Withers <timwithers@gmail.com>
2013-10-08 01:40:12 -0700

    [FIX] Updated jquery version

--- setup/index.php
+++ setup/index.php
@@ -119,7 +119,7 @@
 		}
 		</style>
 		
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	</head>
 	<body>
 	<div id="outer">
